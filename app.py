23026562bc7f6a2da8e1d997a7030b40fad2bd12
kyashasri <168709831+kyashasri@users.noreply.github.com>
2026-03-23 22:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -931,4 +931,5 @@ def check_user():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    print("Starting server...")
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
