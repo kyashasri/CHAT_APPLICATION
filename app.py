ea5a6c26d1feff2fe3e383c723b5bb4bc561e69b
kyashasri <168709831+kyashasri@users.noreply.github.com>
2026-03-23 21:26:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
